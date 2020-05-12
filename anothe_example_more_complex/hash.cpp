16359b8df2875f0f54b4207f3075c16ec0d44e82
winwood <winwood@mail.ru>
2020-05-12 10:44:06 +0300

    refactoring hash module and add class StringHash

--- anothe_example_more_complex/hash.cpp
+++ anothe_example_more_complex/hash.cpp
@@ -2,12 +2,10 @@
 #include <cwchar>
 #include "hash.h"
 
-FileHash::FileHash(const char* fname, HashAlg alg): /*f(fname, std::ios::binary), */buf(new unsigned char[BUF_SIZE])
-{
-    f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    f.open(fname, std::ios::binary);
-    f.exceptions(std::ifstream::goodbit );
+//////////////////////// StringHash ////////////////////////////////////////////
 
+StringHash::StringHash(HashAlg alg): buf(new unsigned char[BUF_SIZE])
+{
     status = BCryptOpenAlgorithmProvider(&hAlgorithm, AlgList[alg], nullptr, 0);
     if (status != 0)  //Open provider?
         throw std::system_error(status, std::system_category(), "Error open crypto algorithm provider");
@@ -16,17 +14,76 @@ FileHash::FileHash(const char* fname, HashAlg alg): /*f(fname, std::ios::binary)
         throw std::system_error(status, std::system_category(), "Error create hash object");
 }
 
-FileHash::~FileHash()
+StringHash::~StringHash()
 {
+    delete[] buf;
     BCryptDestroyHash(hHash);            //destroy hash handle
     BCryptCloseAlgorithmProvider(hAlgorithm,0);  //close provider
-    delete[] buf;
 }
 
-std::string FileHash::calcHash()
+std::string StringHash::name()
+{
+    wchar_t * p = (wchar_t*)buf;
+    std::string res;
+    status = BCryptGetProperty(hAlgorithm, BCRYPT_ALGORITHM_NAME, buf, BUF_SIZE, &len, 0);
+    if (status != 0)
+        throw  std::system_error(status, std::system_category(), "Error get algorithm name");
+    for (ULONG i = 0; p[i]!=L'\0'; i++) {
+        res.push_back(wctob(p[i]));
+    }
+    return res;
+}
+
+void StringHash::finishHash()
+{
+//get hash length
+    status = BCryptGetProperty(hAlgorithm, BCRYPT_HASH_LENGTH, buf, BUF_SIZE, &len, 0);
+    if (status != 0)
+        throw  std::system_error(status, std::system_category(), "Error get hash length");
+    len = *(PULONG)buf;
+//finalize hash value
+    status = BCryptFinishHash(hHash, buf, len, 0);
+    if (status != 0)
+        throw  std::system_error(status, std::system_category(), "Error get hash value");
+}
+
+std::string StringHash::toString()
 {
     const char HEX_DIG[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-    ULONG len;
+//convert hash to string
+    std::string HashStr;
+    for (unsigned int i = 0; i < len; i++) {
+        HashStr.push_back(HEX_DIG[buf[i] >> 4]);    //high forth bits
+        HashStr.push_back(HEX_DIG[buf[i] & 0xf]);   //low forth bits
+    }
+    return HashStr;
+
+}
+
+void StringHash::calcHash(const std::string data)
+{
+//calc hash of
+    status = BCryptHashData(hHash, (PUCHAR)data.c_str(), data.length(), 0);
+    if (status != 0)    //calc hash for data portion
+        throw  std::system_error(status, std::system_category(), "Error evaluate hash data");
+}
+
+std::string  StringHash::operator()(const std::string data)
+{
+    calcHash(data);
+    finishHash();
+    return toString();
+}
+
+//////////////////////// FileHash ////////////////////////////////////////////
+
+void FileHash::calcHash(const std::string data)
+{
+//open file
+    f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    f.open(data, std::ios::binary);
+    f.exceptions(std::ifstream::goodbit );
+
 //calc hash of file
     while (f) {
         f.read( (char*)buf, BUF_SIZE);              //read data portion from file
@@ -35,39 +92,5 @@ std::string FileHash::calcHash()
         if (status != 0)    //calc hash for data portion
             throw  std::system_error(status, std::system_category(), "Error evaluate hash data");
     }
-
-//get hash
-    status = BCryptGetProperty(hAlgorithm, BCRYPT_HASH_LENGTH, buf, BUF_SIZE, &len, 0);
-    if (status != 0)
-        throw  std::system_error(status, std::system_category(), "Error get hash length");
-    len = *(PULONG)buf;
-
-    status = BCryptFinishHash(hHash, buf, len, 0);
-    if (status != 0)
-        throw  std::system_error(status, std::system_category(), "Error get hash value");
-
-//convert hash to string
-    std::string HashStr;
-    for (unsigned int i = 0; i < len; i++) {
-        HashStr.push_back(HEX_DIG[buf[i] >> 4]);    //high forth bits
-        HashStr.push_back(HEX_DIG[buf[i] & 0xf]);   //low forth bits
-    }
-
-    return HashStr;
 }
 
-std::string FileHash::name()
-{
-    ULONG len;
-    wchar_t * p = (wchar_t*)buf;
-    std::string res;
-    status = BCryptGetProperty(hAlgorithm, BCRYPT_ALGORITHM_NAME, buf, BUF_SIZE, &len, 0);
-    if (status != 0)
-        throw  std::system_error(status, std::system_category(), "Error get algorithm name");
-
-    for (ULONG i = 0; p[i]!=L'\0'; i++) {
-        res.push_back(wctob(p[i]));
-    }
-    return res;
-
-}
--- anothe_example_more_complex/hash.h
+++ anothe_example_more_complex/hash.h
@@ -6,33 +6,45 @@
 #include <windows.h>
 
 
-
-class FileHash {
+class StringHash {
 public:
     enum HashAlg {MD2, MD4, MD5, SHA1, SHA256, SHA384, SHA512};
-
-private:
+protected:
+    static constexpr int BUF_SIZE = 1024*4;
+    unsigned char * buf;
+    ULONG len;
     std::map<int,const wchar_t*> AlgList {
         std::pair<int,const wchar_t*>(MD5,BCRYPT_MD5_ALGORITHM),
         std::pair<int,const wchar_t*>(MD2,BCRYPT_MD2_ALGORITHM),
         std::pair<int,const wchar_t*>(MD4,BCRYPT_MD4_ALGORITHM),
         std::pair<int,const wchar_t*>(SHA1,BCRYPT_SHA1_ALGORITHM),
-        std::pair<int,const wchar_t*>(SHA1,BCRYPT_SHA1_ALGORITHM),
         std::pair<int,const wchar_t*>(SHA256,BCRYPT_SHA256_ALGORITHM),
         std::pair<int,const wchar_t*>(SHA384,BCRYPT_SHA384_ALGORITHM),
         std::pair<int,const wchar_t*>(SHA512,BCRYPT_SHA512_ALGORITHM)
     };
-    static constexpr int BUF_SIZE = 1024*4;
-    std::ifstream f;
-    unsigned char * buf;
     BCRYPT_ALG_HANDLE hAlgorithm;
     BCRYPT_HASH_HANDLE hHash;
     NTSTATUS status;
+    virtual void calcHash(const std::string data);
+    virtual void finishHash();
+    virtual std::string toString();
+
+public:
+    StringHash()=delete;
+    StringHash(HashAlg alg = MD5);
+    virtual ~StringHash();
+    std::string operator()(const std::string data);
+    std::string name();
+};
+
+class FileHash:public StringHash {
+
+private:
+    std::ifstream f;
+    virtual void calcHash(const std::string data);
 public:
     FileHash()=delete;
-    FileHash(const char* fname, HashAlg alg = MD5);
-    ~FileHash();
-    std::string calcHash();
-    std::string name();
+    FileHash(HashAlg alg = MD5):StringHash(alg) {}
+    virtual ~FileHash() {}
 };
 
